dd7296b09852195eeda9051835a2dbe14415024b
Jingga69 <hyang.jagadnata@gmail.com>
2025-12-26 14:07:54 +0000

    add scope-minimized manual hooks v1.6

--- fs/open.c
+++ fs/open.c
@@ -451,14 +451,15 @@ out:
 }
 
 #ifdef CONFIG_KSU
- extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
- 			                    int *flags);
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
 #endif
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
 #ifdef CONFIG_KSU
- 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif
 	return do_faccessat(dfd, filename, mode);
 }
